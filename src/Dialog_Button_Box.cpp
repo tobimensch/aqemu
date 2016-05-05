434f6e0ddcbed0195f4ab7a2f0bf9a80ca0631bb
Tobias Gläßer <tobimensch@yahoo.de>
2016-05-05 16:52:01 +0200

    Added missing sources

---
+++ src/Dialog_Button_Box.cpp
@@ -0,0 +1,51 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 Tobias Gläßer
+**
+** This file is part of AQEMU.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA  02110-1301, USA.
+**
+****************************************************************************/
+
+#include <QPushButton>
+
+#include "Dialog_Button_Box.h"
+
+Dialog_Button_Box::Dialog_Button_Box(QWidget* parent) : QDialogButtonBox(parent)
+{
+}
+
+Dialog_Button_Box::Dialog_Button_Box() : QDialogButtonBox()
+{
+}
+
+void Dialog_Button_Box::init()
+{
+    button(QDialogButtonBox::Ok)->setIcon(QIcon(":/ok.png"));
+    button(QDialogButtonBox::Cancel)->setIcon(QIcon(":/cancel.png"));
+}
+
+Dialog_Button_Box::~Dialog_Button_Box()
+{
+}
+
+void Dialog_Button_Box::paintEvent(QPaintEvent* e)
+{
+    init();
+
+    e->accept();
+}
+
---
+++ src/Dialog_Button_Box.h
@@ -0,0 +1,55 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 Tobias Gläßer
+**
+** This file is part of AQEMU.
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor,
+** Boston, MA  02110-1301, USA.
+**
+****************************************************************************/
+
+#ifndef DIALOG_BUTTON_BOX_H
+#define DIALOG_BUTTON_BOX_H
+
+#include <QDialogButtonBox>
+#include <QListWidget>
+#include <QWheelEvent>
+#include <QList>
+#include <QMap>
+
+class QTabWidget;
+class QStackedWidget;
+class QBoxLayout;
+class QSize;
+class QSplitter;
+class QPaintEvent;
+
+class Dialog_Button_Box : public QDialogButtonBox
+{
+    Q_OBJECT
+
+    public:
+        Dialog_Button_Box(QWidget*);
+        Dialog_Button_Box();
+        ~Dialog_Button_Box();
+
+    private:
+        void init();
+        void paintEvent(QPaintEvent*);
+};
+
+#endif
+
+
