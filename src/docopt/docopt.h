e17120ee1ab92bc249677894b9f51970cc7d08ae
Tobias Gläßer <tobimensch@yahoo.de>
2020-05-02 15:54:48 +0200

    docopt update

--- src/docopt/docopt.h
+++ src/docopt/docopt.h
@@ -14,6 +14,32 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <stdexcept>
+
+#ifdef DOCOPT_HEADER_ONLY
+    #define DOCOPT_INLINE inline
+    #define DOCOPT_API
+#else 
+    #define DOCOPT_INLINE
+
+    // With Microsoft Visual Studio, export certain symbols so they 
+    // are available to users of docopt.dll (shared library). The DOCOPT_DLL
+    // macro should be defined if building a DLL (with Visual Studio),
+    // and by clients using the DLL. The CMakeLists.txt and the
+    // docopt-config.cmake it generates handle this.
+    #ifdef DOCOPT_DLL
+        // Whoever is *building* the DLL should define DOCOPT_EXPORTS.
+        // The CMakeLists.txt that comes with docopt does this.
+        // Clients of docopt.dll should NOT define DOCOPT_EXPORTS.
+        #ifdef DOCOPT_EXPORTS
+            #define DOCOPT_API __declspec(dllexport)
+        #else
+            #define DOCOPT_API __declspec(dllimport)
+        #endif
+    #else
+        #define DOCOPT_API
+    #endif
+#endif
 
 namespace docopt {
 	
@@ -24,10 +50,10 @@ namespace docopt {
 	struct DocoptArgumentError : std::runtime_error { using runtime_error::runtime_error; };
 	
 	// Arguments contained '--help' and parsing was aborted early
-	struct DocoptExitHelp : std::runtime_error { DocoptExitHelp(); };
-	
+	struct DocoptExitHelp : std::runtime_error { DocoptExitHelp() : std::runtime_error("Docopt --help argument encountered"){} };
+
 	// Arguments contained '--version' and parsing was aborted early
-	struct DocoptExitVersion : std::runtime_error { DocoptExitVersion(); };
+	struct DocoptExitVersion : std::runtime_error { DocoptExitVersion() : std::runtime_error("Docopt --version argument encountered") {} };
 	
 	/// Parse user options from the given option string.
 	///
@@ -42,7 +68,7 @@ namespace docopt {
 	/// @throws DocoptExitHelp if 'help' is true and the user has passed the '--help' argument
 	/// @throws DocoptExitVersion if 'version' is true and the user has passed the '--version' argument
 	/// @throws DocoptArgumentError if the user's argv did not match the usage patterns
-	std::map<std::string, value> docopt_parse(std::string const& doc,
+	std::map<std::string, value> DOCOPT_API docopt_parse(std::string const& doc,
 					    std::vector<std::string> const& argv,
 					    bool help = true,
 					    bool version = true,
@@ -55,11 +81,15 @@ namespace docopt {
 	///  * DocoptExitHelp - print usage string and terminate (with exit code 0)
 	///  * DocoptExitVersion - print version and terminate (with exit code 0)
 	///  * DocoptArgumentError - print error and usage string and terminate (with exit code -1)
-	std::map<std::string, value> docopt(std::string const& doc,
+	std::map<std::string, value> DOCOPT_API docopt(std::string const& doc,
 					    std::vector<std::string> const& argv,
 					    bool help = true,
 					    std::string const& version = {},
 					    bool options_first = false) noexcept;
 }
+
+#ifdef DOCOPT_HEADER_ONLY
+    #include "docopt.cpp"
+#endif
 
 #endif /* defined(docopt__docopt_h_) */
